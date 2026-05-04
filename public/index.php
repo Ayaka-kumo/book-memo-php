3a994c3ca8b8bfed4acaebb8b7802a2cdb7cf3b2
あなたの名前 <amoulikana@gmail.com>
2026-05-04 15:58:08 +0900

    memo-listにcss読み込んだ

--- public/index.php
+++ public/index.php
@@ -11,6 +11,7 @@ $memos = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>読書メモ一覧</title>
 </head>
 
